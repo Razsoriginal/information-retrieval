1fd722280bdf8dee89102d20695dbc539ef5b2e0
Sayyed Raza <razsoriginals@gmail.com>
2024-04-06 04:14:08 +0530

    updated cosine

--- Cosine.py
+++ Cosine.py
@@ -1,33 +1,42 @@
-import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+ 
+def remove_StopWords(doc):
+    swords = set(stopwords.words('english'))
+    with open(doc, 'r', encoding='utf-8') as file:
+        data = file.read()
+    words = re.sub(r'[^a-zA-Z\s]', '', data).split()
+    ans = ""
+    for w in words:
+        if w.lower() not in swords:
+            ans += w + " "
+    return ans
+ 
+def get_word_frequency(doc, word):
+    with open(doc, 'r', encoding="utf-8") as file:
+        data = file.read()
+    words = re.sub(r'[^a-zA-Z\s]', '', data).split()
+    fdist = FreqDist(words)
+    return str(fdist[word])
 
-# Sample user-item interaction matrix
-data = {
-    'User1': [5, 4, 0, 0, 1],
-    'User2': [0, 2, 3, 4, 5],
-    'User3': [1, 0, 5, 2, 3],
-    'User4': [4, 0, 2, 3, 0]
-}
+doc_1 = "C:/Users/aakash/Videos/IR/IR/Similairty_cosine/para1.txt"
+doc_2 = "C:/Users/aakash/Videos/IR/IR/Similairty_cosine/para2.txt"
+doc_3 = "C:/Users/aakash/Videos/IR/IR/Similairty_cosine/para3.txt"
 
-df = pd.DataFrame(data)
+ans1 = remove_StopWords(doc_1)
+ans2 = remove_StopWords(doc_2)
+ans3 = remove_StopWords(doc_3)
+ 
+main = list(set(ans1.split() + ans2.split() + ans3.split()))
 
-# Calculate cosine similarity between users
-user_similarity = cosine_similarity(df)
-#pip install scikit-learn
-# Function to get recommendations for a target user
-def get_recommendations(target_user):
-    similar_users = user_similarity[target_user]
-    ranked_users = sorted(range(len(similar_users)), key=similar_users.__getitem__, reverse=True)
-    
-    recommendations = []
-    
-    for user in ranked_users:
-        if user != target_user:
-            recommendations.extend(df.iloc[user][df.iloc[target_user] == 0].index.tolist())
-    
-    return list(set(recommendations))
+# Print header
+print(f"{'Word': <10} {'Document 1': <10} {'Document 2': <10} {'Document 3': <10}")
 
-# Example usage
-target_user = 0
-recommendations = get_recommendations(target_user)
-print("Recommendations for User", target_user + 1, ":", recommendations) 
+# Print word frequencies
+for w in main:
+    print(f"{w : <10}", end="")
+    print(f"{get_word_frequency(doc_1, w) : <10}", end="")
+    print(f"{get_word_frequency(doc_2, w) : <10}", end="")
+    print(f"{get_word_frequency(doc_3, w) : <10}")
